14334e432c913daba9dde1515c00c9c49e089c08
adrienjt <adrienjt@users.noreply.github.com>
2019-02-16 13:02:01 -0800

    rm sleep hack in e2e test
    Signed-off-by: adrienjt <adrienjt@users.noreply.github.com>

--- test/e2e/tear_down.sh
+++ test/e2e/tear_down.sh
@@ -1,5 +1,3 @@
 set -euo pipefail
 
-kustomize build test/e2e/cluster1/ | kubectl --context cluster1 delete -f -
-kustomize build test/e2e/cluster2/ | kubectl --context cluster2 delete -f -
 kubectl config use-context cluster1 && skaffold delete -f test/e2e/cluster1/skaffold.yaml
--- test/e2e/test.sh
+++ test/e2e/test.sh
@@ -1,13 +1,14 @@
 set -euo pipefail
 
-# TODO: check intermediate results to pin-point potential issue
-# kubectl --context cluster1 get secret -l multicluster.admiralty.io/service-account-import.name=cluster2-default-pod-lister -o yaml
-# kubectl --context cluster1 get pod -l job-name=multicluster-client -o yaml
+kubectl --context cluster1 label ns default multicluster-service-account=enabled
+kustomize build test/e2e/cluster2/ | kubectl --context cluster2 apply -f -
+kustomize build test/e2e/cluster1/ | kubectl --context cluster1 apply -f -
 
 POD_NAME_2=$(kubectl --context cluster2 get pod -o jsonpath={.items[0].metadata.name})
+echo "waiting for test job to complete..."
 kubectl --context cluster1 wait job/multicluster-client --for condition=complete
 POD_NAME_1=$(kubectl --context cluster1 logs job/multicluster-client | tail -1)
-if [ $POD_NAME_1 == $POD_NAME_2 ]
+if [ "$POD_NAME_1" == "$POD_NAME_2" ]
 then
 	echo "SUCCESS"
 	exit 0
@@ -15,3 +16,7 @@ else
 	echo "FAILURE"
 	exit 1
 fi
+
+kustomize build test/e2e/cluster1/ | kubectl --context cluster1 delete -f -
+kustomize build test/e2e/cluster2/ | kubectl --context cluster2 delete -f -
+kubectl --context cluster1 label ns default multicluster-service-account-
