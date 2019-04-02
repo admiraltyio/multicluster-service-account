1b724b5b9a2aa5f4d0f054f6d0c5bbce5d0278a9
adrienjt <adrienjt@users.noreply.github.com>
2019-04-01 22:28:32 -0700

    run as non root, fix #2
    Signed-off-by: adrienjt <adrienjt@users.noreply.github.com>

--- test/e2e/setup_clusters.sh
+++ test/e2e/setup_clusters.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 PROJECT=$(gcloud config get-value project)
 REGION=$(gcloud config get-value compute/zone)
 for NAME in cluster1 cluster2; do
-	gcloud container clusters create $NAME --preemptible
+	gcloud beta container clusters create $NAME --preemptible --enable-pod-security-policy
 	gcloud container clusters get-credentials $NAME
 	CONTEXT=gke_$PROJECT"_"$REGION"_"$NAME
 	sed -i -e "s/$CONTEXT/$NAME/g" ~/.kube/config
 	kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
 	kubectl cluster-info
+	kubectl apply -f test/e2e/must-run-as-non-root.yaml
 done
